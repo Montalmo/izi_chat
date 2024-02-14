f47006235009680b24062fd5271fc6ae324d71d0
Montalmo <photowedg@gmail.com>
2024-02-14 13:40:14 +0200

    Start Develope - Chating screen: Add bottom area with text input and additional buttons

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -30,7 +30,119 @@ class _ChatScreenState extends State<ChatScreen> {
             mqd,
           ),
         ),
-        body: Center(child: Text('User: ${dialogChatUser.name}')),
+        body: Column(
+          children: [
+            Expanded(
+              child: StreamBuilder(
+                builder: (context, snapshot) {
+                  switch (snapshot.connectionState) {
+                    case ConnectionState.waiting:
+                    case ConnectionState.active:
+                    case ConnectionState.none:
+                    // return const Center(
+                    //   child: CircularProgressIndicator(),
+                    // );
+                    case ConnectionState.done:
+                      final List _messageList = [];
+                      if (_messageList.isNotEmpty) {
+                        return ListView.builder(
+                            itemCount: _messageList.length,
+                            itemBuilder: (context, index) {
+                              Text('Message: ${_messageList[index]}');
+                            });
+                      } else {
+                        return const Center(
+                          child: Text('Say Hi 🖐️! To new user!😜'),
+                        );
+                      }
+                      
+                  }
+                },
+                stream: null,
+              ),
+            ),
+            _chatInput(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _chatInput() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 4.0,
+      ),
+      child: Row(
+        children: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.emoji_emotions_outlined,
+              color: PalletColors.cGray,
+            ),
+          ),
+          const Expanded(
+            child: TextField(
+              keyboardType: TextInputType.multiline,
+              maxLength: null,
+              decoration: InputDecoration(
+                  hintStyle: TextStyle(
+                    color: PalletColors.cGrayText,
+                    fontSize: 14.0,
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.transparent),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(
+                        14.0,
+                      ),
+                    ),
+                  ),
+                  contentPadding: EdgeInsets.symmetric(
+                    vertical: 0,
+                    horizontal: 12,
+                  ),
+                  border: OutlineInputBorder(),
+                  filled: true,
+                  fillColor: PalletColors.cGrayField,
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.transparent),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(
+                        14.0,
+                      ),
+                    ),
+                  ),
+                  hintText: 'Type Something ...'),
+            ),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.image_outlined,
+              size: 24,
+              color: PalletColors.cGray,
+            ),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.camera_outlined,
+              size: 24,
+              color: PalletColors.cGray,
+            ),
+          ),
+          IconButton.filled(
+            onPressed: () {},
+            color: PalletColors.cCyan600,
+            icon: const Icon(
+              Icons.send_outlined,
+              size: 18,
+              color: PalletColors.cWhite,
+            ),
+          ),
+        ],
       ),
     );
   }
---
+++ lib/widgets/message_card.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MessageCrd extends StatefulWidget {
+  const MessageCrd({super.key});
+
+  @override
+  State<MessageCrd> createState() => _MessageCrdState();
+}
+
+class _MessageCrdState extends State<MessageCrd> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
